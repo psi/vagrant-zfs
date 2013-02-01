c3defb4fbf3e570e473ff2cf0d18d72979103b78
JD Harrington <jd@jdharrington.net>
2013-01-31 22:29:52 -0500

    fix reference to spec_helper

--- test/unit/middleware/clone_zfs_test.rb
+++ test/unit/middleware/clone_zfs_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.dirname(__FILE__) + "/../../spec_helper"
 require "vagrant_zfs/middleware/clone_zfs"
 
 describe VagrantZFS::Middleware::CloneZFS do
